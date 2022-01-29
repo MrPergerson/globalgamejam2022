7c07e923a2630ccd1a929952f0567d3ca4654d2d
Cole Pergerson <cole.pergerson@gmail.com>
2022-01-28 21:08:38 -0800

    Added basic move

--- globalgamejam2022/Assets/Robot.cs
+++ globalgamejam2022/Assets/Robot.cs
@@ -6,10 +6,12 @@ public class Robot : MonoBehaviour
 {
     Player player;
     Rigidbody2D rb;
+    [SerializeField] private float speed = .5f;
 
     private void Awake()
     {
         player = FindObjectOfType<Player>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Start()
@@ -18,13 +20,14 @@ public class Robot : MonoBehaviour
     }
 
 
-    void Update()
+    void FixedUpdate()
     {
         Vector2 heading = player.transform.position - transform.position;
-        var distance = heading.magnitude;
+        var distance = heading.magnitude; // this is a slow calcuation
         var direction = heading / distance;
 
-        //rb.MovePosition
+        if(distance > .5)
+            rb.MovePosition((Vector2)transform.position + direction * speed * Time.fixedDeltaTime);
 
     }
 }
