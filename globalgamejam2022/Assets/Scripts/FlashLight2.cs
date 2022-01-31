bb3552fc194299384e343916f5c320e799a96dbb
shahbazmansahia <shahbaz_mansahia@yahoo.com>
2022-01-31 01:08:48 -0800

    Restored gamepad functionality
    - Gamepad now works again! fixed it by adding gamepad functionality loosely based upon how to mouse torch system already works (it was a lot easier to implement this since we don't need the mouse fromScreen->worldPos translation)

--- globalgamejam2022/Assets/Scripts/FlashLight2.cs
+++ globalgamejam2022/Assets/Scripts/FlashLight2.cs
@@ -6,10 +6,18 @@ public class FlashLight2 : MonoBehaviour
 {
     [SerializeField] Camera camera;
     PlayerControls playerControls;
-
+    
+    // attribs for gamepad functionality
+    // drag and drop the Player game object in inspector into player field
+    public GameObject player;
+    private Player playerController;
+    private Vector2 look;
+    private float angleToRAnalogPosition;
+    private bool isGamePadEnabled;
     void Awake()
     {
         playerControls = new PlayerControls();
+        playerController = player.GetComponent<Player>();
     }
 
     private void OnEnable()
@@ -25,18 +33,35 @@ public class FlashLight2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        if(GameManager.Instance.isPaused != true)
+        isGamePadEnabled = playerController.usingGamepad;
+        // executes if the gamepad is not the currently used device in input system
+        if (isGamePadEnabled)
         {
-            Vector2 mouseScreenPosition = playerControls.Player.Look.ReadValue<Vector2>();
-            Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(mouseScreenPosition);
+            if (GameManager.Instance.isPaused != true)
+            {
+                Vector2 mouseScreenPosition = playerControls.Player.Look.ReadValue<Vector2>();
+                Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(mouseScreenPosition);
 
-            Vector3 directionToMousePosition = mouseWorldPosition - transform.position;
-            float angleToMousePosition = Mathf.Atan2(directionToMousePosition.y, directionToMousePosition.x);
-            angleToMousePosition = angleToMousePosition * Mathf.Rad2Deg;
+                Vector3 directionToMousePosition = mouseWorldPosition - transform.position;
+                float angleToMousePosition = Mathf.Atan2(directionToMousePosition.y, directionToMousePosition.x);
+                angleToMousePosition = angleToMousePosition * Mathf.Rad2Deg;
 
-            transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angleToMousePosition));
+                transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angleToMousePosition));
+
+            }
+        }
+        // executes if gamePad is the currently used device in input system
+        else
+        {
+            // Gets the value from user input
+            look = playerControls.Player.Look.ReadValue<Vector2>();
+            
+            angleToRAnalogPosition = Mathf.Atan2(look.y, look.x);
+            angleToRAnalogPosition = angleToRAnalogPosition * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angleToRAnalogPosition));
 
         }
+
     }
+    
 }
